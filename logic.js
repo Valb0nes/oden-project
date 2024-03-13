395f8eb1e97342911bab94ab3658494585edf172
Val <143848541+Valb0nes@users.noreply.github.com>
2024-03-13 14:47:35 -0700

    js, css, html
    Put in JavaScript book array and added an html document and CSS document

--- logic.js
+++ logic.js
@@ -1,7 +1,20 @@
 
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read; 
+}
 
 
+function addBookToLibrary(title, author, pages, read) {
+let book = new Book(title, author, pages, read);
+myLibrary.push(book)
+}
 
+addBookToLibrary("A Court of Thorns and Roses", "Sarah J. Maas", 432, true) 
 
-
-ACourtofThornsandRoses.info();
+addBookToLibrary("A Court of Mist and Fury", "Sarah J. Maas", 650, true) 
+console.log(myLibrary)
